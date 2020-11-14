aa94b9f2cafebeea5bde73cb0c48b14ad2ea66af
Ankit Boora <43762127+Ankit-Boora@users.noreply.github.com>
2020-11-14 15:13:18 +0530

    Via Computer
    These files upload directly from my computer.

---
+++ javascript.js
@@ -0,0 +1,15 @@
+		alert("Welcome to my site. My name is Anki and I am SEO and website Development expert.");
+
+		function changeContent() {
+					document.getElementById("sample").innerHTML = "SEO Expert and Website Developer.";
+			}
+		function getvalue() {
+					alert(document.getElementById('demo').value);
+			}
+				
+		function getvalue() {
+					alert(document.getElementById('demo').checked);
+			}
+		
+			
+		
