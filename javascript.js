381ba5278cb5c7543443243277a62ddf57ead6bf
Ankit Boora <43762127+Ankit-Boora@users.noreply.github.com>
2020-11-16 20:13:24 +0530

    Add more javascripts

--- javascript.js
+++ javascript.js
@@ -3,13 +3,30 @@
 		function changeContent() {
 					document.getElementById("sample").innerHTML = "SEO Expert and Website Developer.";
 			}
-		function getvalue() {
-					alert(document.getElementById('demo').value);
+		function getValue() {
+					alert(document.getElementById("demo").value);
 			}
 				
-		function getvalue() {
-					alert(document.getElementById('demo').checked);
+		function getValue() {
+					alert(document.getElementById("demo").checked);
 			}
-		
+		function readFile() {
+					var reader = new FileReader();
+					var file = document.getElementById("demo").files[0];
+					
+					reader.onload = function(e) {
+						document.getElementById("result").src = e.target.result;
+			}
+					reader.readAsDataURL(file);
+			}
+		function getValue() {
+					var radios = document.getElementsByName('gender');
+					var number_of_radios = radios.length;
+					
+					for(var i = 0; i <number_of_radios; i++) {
+						if(radios[i].checked) {
+							alert(radios[i].value);
+						}
+					}
+			}
 			
-		
