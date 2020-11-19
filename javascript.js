40053afe355e6c541ada3104684c715734172ccf
Ankit Boora <43762127+Ankit-Boora@users.noreply.github.com>
2020-11-19 18:11:25 +0530

    Update javascript.js
    File Update in Javascript File.

--- javascript.js
+++ javascript.js
@@ -1,22 +1,21 @@
 		alert("Welcome to my site. My name is Anki and I am SEO and website Development expert.");
-		alert("Welcome to my site. My name is Anki and I am SEO and website Development expert.");
 
 		function changeContent() {
 					document.getElementById("sample").innerHTML = "SEO Expert and Website Developer.";
 			}
 		function getValue() {
-					alert(document.getElementById('demo').value);
+					alert(document.getElementById("demo").value);
 			}
 				
 		function getValue() {
-					alert(document.getElementById('demo').checked);
+					alert(document.getElementById("demo").checked);
 			}
 		function readFile() {
 					var reader = new FileReader();
-					var file = document.getElementById('demo').files[0];
+					var file = document.getElementById("demo").files[0];
 					
 					reader.onload = function(e) {
-						document.getElementById('result').src = e.target.result;
+						document.getElementById("result").src = e.target.result;
 			}
 					reader.readAsDataURL(file);
 			}
@@ -30,35 +29,23 @@
 						}
 					}
 			}
-			
-
-		function changeContent() {
-					document.getElementById("sample").innerHTML = "SEO Expert and Website Developer.";
-			}
 		function getValue() {
 					alert(document.getElementById('demo').value);
 			}
-				
 		function getValue() {
-					alert(document.getElementById('demo').checked);
-			}
-		function readFile() {
-					var reader = new FileReader();
-					var file = document.getElementById('demo').files[0];
-					
-					reader.onload = function(e) {
-						document.getElementById('result').src = e.target.result;
-			}
-					reader.readAsDataURL(file);
-			}
+					var value = document.getElementById('sample').value;
+					alert(value);
 		function getValue() {
-					var radios = document.getElementsByName('gender');
-					var number_of_radios = radios.length;
+					var options = document.getElementById('sample').options;
+					var options_count = document.getElementById('sample').options.length;
+					var value = [];
 					
-					for(var i = 0; i <number_of_radios; i++) {
-						if(radios[i].checked) {
-							alert(radios[i].value);
+					for(var i = 0; i < options_count; i++) {
+						if(options[i].selected) {
+							value.push(options[i].value);
 						}
 					}
+					
+					alert(JSON.stringify(value));
 			}
-			
+		
